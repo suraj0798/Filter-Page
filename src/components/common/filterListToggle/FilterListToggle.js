2e5519843b4ced0559db286249203980f75bd48b
suraj0798 <surajdhas07@gmail.com>
2023-03-02 01:24:46 +0530

    category list update

--- src/components/common/filterListToggle/FilterListToggle.js
+++ src/components/common/filterListToggle/FilterListToggle.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FilterListToggle.css';
-import fashion from 'E:/suraj/filter-page/src/images/fashion.png'
+import ListItems from '../../../constants/Constancts';
+
+
 
 
 const FilterListToggle = () => {
-
+  const [data, setdata] = useState(ListItems);
+  const handleCategoryClick = (itemCategory) => {
+    const result = ListItems.filter((currentData) => {
+      return currentData.category === itemCategory;
+    });
+    setdata(result);
+  }
+  
   return(
     <div className='buttonsGroup'>
         
-       <button type="button"><img className='buttonIcon' src= {fashion} alt='img'/>Fashion</button>
-       <button type="button" aria-label='Mobiles'>📱Mobiles</button>
-       <button type="button">💻Electronics</button>
-       <button type="button">Home</button>
-       <button type="button">Appliances</button>
-       <button type="button">Personal Care</button>
-       <button type="button">Toys & Baby</button>
-       <button type="button">Furniture</button>
-       <button type="button">Sports</button>
-       <button type="button">Grocery</button>
-       <button type="button">Nutrition & More</button>
-       <button type="button">Bikes & Cars</button>
+       <button type="button" onClick={() => handleCategoryClick('fashion')}>
+        <img className='buttonIcon' src= "./images/fashion.png"  alt='img' />
+        <label className='buttonLabel'>Fashion</label>
+      </button>
+       <button type="button"><img className='buttonIcon' src= "./images/mobile.png" alt='img'/><label className='buttonLabel'>Mobiles</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/electronic.png" alt='img'/><label className='buttonLabel'>Electronics</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/home.png"  alt='img'/><label className='buttonLabel'>Home</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/appliance.png" alt='img'/><label className='buttonLabel'>Appliances</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/personalcare.png" alt='img'/><label className='buttonLabel'>Personal Care</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/toy.png" alt='img'/><label className='buttonLabel'>Toys & Baby</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/furniture.png" alt='img'/><label className='buttonLabel'>Furniture</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/sports.png" alt='img'/><label className='buttonLabel'>Sports</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/grocery.png" alt='img'/><label className='buttonLabel'>Grocery</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/nutrition.png" alt='img'/><label className='buttonLabel'>Nutrition & More</label></button>
+       <button type="button"><img className='buttonIcon' src= "./images/vehicle.png" alt='img'/><label className='buttonLabel'>Bikes & Cars</label></button>
     </div>
   )
 }
--- src/components/filterpage/list/List.js
+++ src/components/filterpage/list/List.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './List.css'
+import StarOutlineRoundedIcon from '@mui/icons-material/StarOutlineRounded';
+import ListItems from '../../../constants/Constancts';
 
 const List = () => {
+  const [listData, setListData] = useState(ListItems);
   return (
-    <div>List</div>
+    <div className='cardBox'>List
+      {listData.map((values) => {
+        const {id,title,price,image,rating,description} = values;
+        return(
+          <>
+           <div className="listCard" key={id}>
+        <div className="container">
+        <div className="card">
+            <div className="content-1">
+                <div className="logo-img">
+                    {/* <img src="https://i.postimg.cc/vBJtjtRC/nike-logo.png" alt=""/> */}
+                    
+                </div>
+                <img src={image} alt=""/>
+                
+            </div>
+            <div className="content-2">
+                <div className="branding">
+                    <span>{title}</span>
+                    <span></span>
+                    <span>NEW</span>
+                    <h4>{description}</h4>
+                </div>
+                <div className="ratings">
+                    {rating}
+                    {/* <span><i><StarOutlineRoundedIcon/></i></span>
+                    <span><i><StarOutlineRoundedIcon/></i></span>
+                    <span><i><StarOutlineRoundedIcon/></i></span>
+                    <span><i><StarOutlineRoundedIcon/></i></span>
+                    <span><i><StarOutlineRoundedIcon/></i></span> */}
+                </div>
+                <div className="price">
+                    <span>Rs. {price}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+        </div>
+          </>
+        )
+      })}
+      
+      </div>
+    
   )
 }
 
